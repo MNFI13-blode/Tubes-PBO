959e9a8f65786bd27fbf12f93469f07a4c15b84b
NaufalHanss1 <127006200+NaufalHanss1@users.noreply.github.com>
2024-04-20 17:30:07 +0700

    Add files via upload

---
+++ Project_Tel-U/src/main/java/com/mycompany/project_tel/u/Project_TelU.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project_tel.u;
+
+/**
+ *
+ * @author oyest
+ */
+public class Project_TelU {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
