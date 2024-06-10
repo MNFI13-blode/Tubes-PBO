f3ee9f8cc506253dea1ad92a6608887ea4cd3b23
MNFI13-blode <naufaltaurus24@gmail.com>
2024-06-11 01:51:25 +0700

    update

--- PBO2024REAL/Tubes-PBO/Project_Tel-U/src/main/java/Search/Search.java
+++ PBO2024REAL/Tubes-PBO/Project_Tel-U/src/main/java/Search/Search.java
@@ -189,12 +189,13 @@ public class Search extends javax.swing.JFrame {
         );
 
         jPanel5.setBackground(new java.awt.Color(255, 0, 0));
+        jPanel5.setPreferredSize(new java.awt.Dimension(107, 0));
 
         javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
         jPanel5.setLayout(jPanel5Layout);
         jPanel5Layout.setHorizontalGroup(
             jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 100, Short.MAX_VALUE)
+            .addGap(0, 107, Short.MAX_VALUE)
         );
         jPanel5Layout.setVerticalGroup(
             jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -233,7 +234,7 @@ public class Search extends javax.swing.JFrame {
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 488, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
             .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(jPanel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel5, javax.swing.GroupLayout.DEFAULT_SIZE, 575, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
